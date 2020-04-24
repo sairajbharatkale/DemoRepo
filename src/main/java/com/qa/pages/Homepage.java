f20214e72802c432f94d139da07e34a077ceb99d
unknown <sairaj.bharat.kale@gmail.com>
2020-04-24 16:53:43 +0530

    first git commit for my selenium code

---
+++ src/main/java/com/qa/pages/Homepage.java
@@ -0,0 +1,10 @@
+package com.qa.pages;
+
+public class Homepage {
+	
+	public void homePage()
+	{
+		System.out.println("This is home page");
+	}
+
+}
---
+++ src/test/java/com/qa/tests/LoginTest.java
@@ -0,0 +1,10 @@
+package com.qa.tests;
+
+public class LoginTest {
+	
+	public void loginTest()
+	{
+		System.out.println("This is Login Test");
+	}
+
+}
